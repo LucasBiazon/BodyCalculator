314aec26d8240bd911e65da293ccc5c9d8239ab7
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-16 21:19:10 -0300

    Styles

---
+++ scripts/calculator.js
@@ -0,0 +1,14 @@
+const translate = {
+    en: {
+        typeCalculator: "TypeCalculator",
+        imc: "IMC",
+        corporalDensity: "Corporal Density",
+        dailyEnergyExpenditure: "Daily Energy Expenditure",
+        basalMetabolicRate: "Basal Metabolic Rate",
+        nutritionalNeeds: "Nutritional needs",
+        submit: "Calculate"
+    }, 
+    pt_br: {
+
+    }
+}
