5aa2f89c0b0fe3359c8a61554c25b481b5f8d29c
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-16 21:59:59 -0300

    Init JS

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -12,3 +12,44 @@ const translate = {
 
     }
 }
+
+// Types Calculator
+const optionsBt = document.querySelector("#optionsBt")
+const optionsCalculator = document.querySelector("#optionsCalculator")
+
+optionsBt.addEventListener("click", () => { 
+    optionsCalculator.classList.toggle("disabled")
+})
+
+
+// Lang
+const btLang = document.querySelector("#btLang")
+const optionsLang = document.querySelector("#DivOptionsLang")
+const optionPt_Br = document.querySelector("#optionPt_Br")
+const optionEn = document.querySelector("#optionEn")
+
+btLang.addEventListener("click", () => { 
+    optionsLang.classList.toggle("disabled")
+})
+
+optionPt_Br.addEventListener("click", TranslatePt_Br())
+optionEn.addEventListener("click", TranslateEn())
+
+ function TranslatePt_Br(){
+    
+}
+
+function TranslateEn(){
+}
+
+const loading = document.querySelector("#loading")
+const opIMC = document.querySelector("#opIMC")
+const opDensity = document.querySelector("#opDensity")
+const opEnergy = document.querySelector("#opEnergy")
+const opMetabolic = document.querySelector("#opMetabolic")
+const opNutritional= document.querySelector("#opNutritional")
+const calculator = document.querySelector("#Calculator")
+
+opIMC.addEventListener("click", () => {
+    loading.classList.add("disabled")
+}) 
