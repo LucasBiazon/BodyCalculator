71ea64f4a8fed3ff222890385c1b4299d33fcafe
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-17 00:34:21 -0300

    Option Calculator

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -50,6 +50,45 @@ const opMetabolic = document.querySelector("#opMetabolic")
 const opNutritional= document.querySelector("#opNutritional")
 const calculator = document.querySelector("#Calculator")
 
+const calculatorImc = document.querySelector("#CalculatorIMC")
+const calculatorDensity = document.querySelector("#CalculatorDensity")
+const calculatorEnergy = document.querySelector("#CalculatorEnergy")
+const calculatorMetabolic = document.querySelector("#CalculatorMetabolic")
+const calculatorNutritional = document.querySelector("#CalculatorNutritional")
+let calculatorAtual = loading
+
+
 opIMC.addEventListener("click", () => {
-    loading.classList.add("disabled")
+    calculatorAtual.classList.add("disabled")
+    calculatorImc.classList.remove("disabled")
+    calculatorAtual = calculatorImc    
+    document.querySelector("#title").innerHTML = opIMC.innerHTML
 }) 
+
+opDensity.addEventListener("click", () => {
+    calculatorAtual.classList.add("disabled")
+    calculatorDensity.classList.remove("disabled")
+    calculatorAtual = calculatorDensity
+    document.querySelector("#title").innerHTML =  opDensity.innerHTML
+})
+
+opEnergy.addEventListener("click", () => {
+    calculatorAtual.classList.add("disabled")
+    calculatorEnergy.classList.remove("disabled")
+    calculatorAtual = calculatorEnergy
+    document.querySelector("#title").innerHTML = opEnergy.innerHTML
+})
+
+opMetabolic.addEventListener("click", () => {
+    calculatorAtual.classList.add("disabled")
+    calculatorMetabolic.classList.remove("disabled")
+    calculatorAtual = calculatorMetabolic
+    document.querySelector("#title").innerHTML = opMetabolic.innerHTML
+})
+
+opNutritional.addEventListener("click", () => {
+    calculatorAtual.classList.add("disabled")
+    calculatorNutritional.classList.remove("disabled")
+    calculatorAtual = calculatorNutritional
+    document.querySelector("#title").innerHTML = opNutritional.innerHTML
+})
