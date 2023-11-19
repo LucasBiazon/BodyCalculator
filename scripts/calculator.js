6a39092bc31ba21620dbecf989222df7d871533c
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-18 22:18:41 -0300

    Translate part 1

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,39 @@
+const translate = {
+    en: {
+        typeCalculator: "Type Calculator",
+        setCalculator: "Set a calculator type...",
+        results: "Results",
+        imc: "IMC",
+        corporalDensity: "Corporal Density",
+        dailyEnergyExpenditure: "Daily Energy Expenditure",
+        basalMetabolicRate: "Basal Metabolic Rate",
+        nutritionalNeeds: "Nutritional needs",
+        submit: "Calculate",
+        height: "Height (m):",
+        weight: "Weight (kg):",
+        age: "Age (y):",
+        gender: "Gender",
+        masculine: "Masculine",
+        feminine: "Feminine"
+    }, 
+    pt_br: {
+        typeCalculator: "Tipo de Calculadora",
+        setCalculator: "Defina o tipo de calculadora...",
+        results: "Resultados",
+        imc: "IMC",
+        corporalDensity: "Densidade corporal",
+        dailyEnergyExpenditure: "Gasto energético diário",
+        basalMetabolicRate: "Taxa de metabolismo Basal ",
+        nutritionalNeeds: "Macronutrientes",
+        submit: "Calcular",
+        height: "Altura (m):",
+        weight: "Peso (kg):",
+        age: "Idade (anos):",
+        gender: "Gênero",
+        masculine: "Masculino",
+        feminine: "Feminino"
+    }
+}
 // Lang
 const btLang = document.querySelector("#btLang")
 const DivOptionsLang = document.querySelector("#DivOptionsLang")
@@ -6,6 +42,7 @@ const optionEn = document.querySelector("#optionEn")
 
 // Types Calculator
 const btOptionsCalculator = document.querySelector("#btOptionsCalculator")
+const titleOptionsCalculator = document.querySelector("#titleOptionsCalculator")
 const optionsCalculator = document.querySelector("#optionsCalculator")
 
 const opIMC = document.querySelector("#opIMC")
@@ -23,39 +60,25 @@ const calculatorEnergy = document.querySelector("#CalculatorEnergy")
 const calculatorMetabolic = document.querySelector("#CalculatorMetabolic")
 const calculatorNutritional = document.querySelector("#CalculatorNutritional")
 
-
 // Results
 const results = document.querySelector("#results")
 const loadingResults = document.querySelector("#loadingResults")
 
+// Strings Elements
+const StringBtCalculate = document.querySelectorAll(".btCalculator")
+const StringHeight = document.querySelectorAll(".height")
+const StringWeight = document.querySelectorAll(".weight")
+const StringAge = document.querySelectorAll(".age")
+const StringGender = document.querySelectorAll(".gender")
+const StringMasculine = document.querySelectorAll(".masculine")
+const StringFeminine = document.querySelectorAll(".feminine")
 
-const translate = {
-    en: {
-        typeCalculator: "Type Calculator",
-        setCalculator: "Set a calculator type...",
-        imc: { 
-            title: "IMC",
-            height: "Height (m):",
-            weight: "Weight (kg):",
-            invalid: "Invalid"
-        },
-        corporalDensity: "Corporal Density",
-        dailyEnergyExpenditure: "Daily Energy Expenditure",
-        basalMetabolicRate: "Basal Metabolic Rate",
-        nutritionalNeeds: "Nutritional needs",
-        submit: "Calculate"
-    }, 
-    pt_br: {
-        typeCalculator: "Tipo de Calculadora",
-        setCalculator: "Defina o tipo de calculadora...",
-        imc: { 
-            title: "IMC",
-            height: "Altura (m):",
-            weight: "Peso (kg):",
-            invalid: "Inválido"
-        },
-    }
-}
+
+
+let invalid = translate.en.imc.invalid
+
+
+
 
 // lang events and functions
 btLang.addEventListener("click", () => { 
@@ -67,10 +90,69 @@ optionPt_Br.addEventListener("click", TranslatePt_Br)
 optionEn.addEventListener("click", TranslateEn)
 
 function TranslatePt_Br(){
-    invalid = translate.pt_br.imc.invalid
+    titleOptionsCalculator.innerHTML = translate.pt_br.typeCalculator
+    loading.innerHTML = translate.pt_br.setCalculator
+    loadingResults.innerHTML = translate.pt_br.results
+    opIMC.innerHTML = translate.pt_br.imc
+    opDensity.innerHTML = translate.pt_br.corporalDensity
+    opEnergy.innerHTML = translate.pt_br.dailyEnergyExpenditure
+    opMetabolic.innerHTML = translate.pt_br.basalMetabolicRate
+    opNutritional.innerHTML = translate.pt_br.nutritionalNeeds
+    for(const i of StringBtCalculate){
+        i.innerHTML= translate.pt_br.submit
+    }
+    for(const s of StringHeight){
+        s.innerHTML= translate.pt_br.height
+    }
+    for(const s of StringWeight){
+        s.innerHTML= translate.pt_br.weight
+    }
+    for(const s of StringAge){
+        s.innerHTML= translate.pt_br.age
+    }
+    for(const s of StringGender){
+        s.innerHTML= translate.pt_br.gender
+    }
+    for(const s of StringMasculine){
+        s.innerHTML= translate.pt_br.masculine
+    }
+    for(const s of StringFeminine){
+        s.innerHTML= translate.pt_br.feminine
+    }
+  
 }
 
 function TranslateEn(){
+    titleOptionsCalculator.innerHTML = translate.en.typeCalculator
+    loading.innerHTML = translate.en.setCalculator
+    loadingResults.innerHTML = translate.en.results
+    opIMC.innerHTML = translate.en.imc
+    opDensity.innerHTML = translate.en.corporalDensity
+    opEnergy.innerHTML = translate.en.dailyEnergyExpenditure
+    opMetabolic.innerHTML = translate.en.basalMetabolicRate
+    opNutritional.innerHTML = translate.en.nutritionalNeeds
+    for(const i of StringBtCalculate){
+        i.innerHTML= translate.en.submit
+    }
+    for(const s of StringHeight){
+        s.innerHTML= translate.en.height
+    }
+    for(const s of StringWeight){
+        s.innerHTML= translate.en.weight
+    }
+    for(const s of StringAge){
+        s.innerHTML= translate.en.age
+    }
+    for(const s of StringGender){
+        s.innerHTML= translate.en.gender
+    }
+    for(const s of StringMasculine){
+        s.innerHTML= translate.en.masculine
+    }
+    for(const s of StringFeminine){
+        s.innerHTML= translate.en.feminine
+    }
+  
 }
 
 
@@ -85,40 +167,40 @@ opIMC.addEventListener("click", () => {
     calculatorAtual.classList.add("disabled")
     calculatorImc.classList.remove("disabled")
     calculatorAtual = calculatorImc    
-    document.querySelector("#titleOptionsCalculator").innerHTML = opIMC.innerHTML
+    titleOptionsCalculator.innerHTML = opIMC.innerHTML
 }) 
 
 opDensity.addEventListener("click", () => {
     calculatorAtual.classList.add("disabled")
     calculatorDensity.classList.remove("disabled")
     calculatorAtual = calculatorDensity
-    document.querySelector("#titleOptionsCalculator").innerHTML =  opDensity.innerHTML
+    titleOptionsCalculator.innerHTML =  opDensity.innerHTML
 })
 
 opEnergy.addEventListener("click", () => {
     calculatorAtual.classList.add("disabled")
     calculatorEnergy.classList.remove("disabled")
     calculatorAtual = calculatorEnergy
-    document.querySelector("#titleOptionsCalculator").innerHTML = opEnergy.innerHTML
+    titleOptionsCalculator.innerHTML = opEnergy.innerHTML
 })
 
 opMetabolic.addEventListener("click", () => {
     calculatorAtual.classList.add("disabled")
     calculatorMetabolic.classList.remove("disabled")
     calculatorAtual = calculatorMetabolic
-    document.querySelector("#titleOptionsCalculator").innerHTML = opMetabolic.innerHTML
+    titleOptionsCalculator.innerHTML = opMetabolic.innerHTML
 })
 
 opNutritional.addEventListener("click", () => {
     calculatorAtual.classList.add("disabled")
     calculatorNutritional.classList.remove("disabled")
     calculatorAtual = calculatorNutritional
-    document.querySelector("#titleOptionsCalculator").innerHTML = opNutritional.innerHTML
+    titleOptionsCalculator.innerHTML = opNutritional.innerHTML
 })
 
 // Calculator operation
 calculator.addEventListener("submit", (event) => event.preventDefault())
-let invalid = translate.en.imc.invalid
+
 
 function CalculateImc() {
    const height = parseFloat(document.querySelector(" #CalculatorIMC #height").value) 
