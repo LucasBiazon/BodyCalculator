2977c976c059ebe0ceee8eb928d1fb7985326434
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-19 03:34:03 -0300

    70%

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -3,7 +3,16 @@ const translate = {
         typeCalculator: "Type Calculator",
         setCalculator: "Set a calculator type...",
         results: "Results",
+        invalid: "Invalid",
         imc: "IMC",
+
+        imcThinness: "Thinness",
+        imcNormal: "Normal",
+        imcOverweight: "Overweight",
+        imcObesity: "Obesity",
+        imcObesityS:"Severe obesity",
+        imcSlogan: "Your IMC are ",
+
         corporalDensity: "Corporal Density",
         dailyEnergyExpenditure: "Daily Energy Expenditure",
         basalMetabolicRate: "Basal Metabolic Rate",
@@ -30,7 +39,16 @@ const translate = {
         typeCalculator: "Tipo de Calculadora",
         setCalculator: "Defina o tipo de calculadora...",
         results: "Resultados",
+        invalid: "Inválido",
+
         imc: "IMC",
+        imcThinness: "Magreza",
+        imcNormal: "Normal",
+        imcOverweight: "Sobrepeso",
+        imcObesity: "Obesidade",
+        imcObesityS:"Obesidade severa",
+        imcSlogan: "Seu IMC é de ",
+
         corporalDensity: "Densidade corporal",
         dailyEnergyExpenditure: "Gasto energético diário",
         basalMetabolicRate: "Taxa de metabolismo Basal ",
@@ -88,6 +106,7 @@ const titleCalculatorNutritional = document.querySelector("#titleCalculatorNutri
 // Results
 const results = document.querySelector("#results")
 const loadingResults = document.querySelector("#loadingResults")
+let pResult = document.createElement("p")
 
 // Strings Elements
 
@@ -108,7 +127,16 @@ const StringCalories = document.querySelector(".calories")
 const StringCarbohydrates = document.querySelector(".carbohydrates")
 const StringProtein = document.querySelector(".protein")
 const StringFat = document.querySelector(".fat")
-let invalid = translate.en.imc.invalid
+
+//IMC 
+let  StringimcSlogan = translate.en.imcSlogan
+let StringimcThinness = translate.en.imcThinness
+let  StringimcNormal = translate.en.imcNormal
+let  StringimcOverweight = translate.en.imcOverweight
+let StringimcObesity = translate.en.imcObesity
+let StringimcObesityS = translate.en.imcObesityS
+
+let  Stringinvalid = translate.en.invalid
 
 
 
@@ -123,23 +151,36 @@ optionPt_Br.addEventListener("click", TranslatePt_Br)
 optionEn.addEventListener("click", TranslateEn)
 
 function TranslatePt_Br(){
+    
     titleOptionsCalculator.innerHTML = translate.pt_br.typeCalculator
     loading.innerHTML = translate.pt_br.setCalculator
     loadingResults.innerHTML = translate.pt_br.results
+
     titleCalculatorImc.innerHTML = translate.pt_br.imc
     titleCalculatorDensity.innerHTML = translate.pt_br.corporalDensity
     titleCalculatorEnergy.innerHTML = translate.pt_br.dailyEnergyExpenditure
     titleCalculatorMetabolic.innerHTML = translate.pt_br.basalMetabolicRate
     titleCalculatorNutritional.innerHTML = translate.pt_br.nutritionalNeeds
+
     opIMC.innerHTML = translate.pt_br.imc
     opDensity.innerHTML = translate.pt_br.corporalDensity
     opEnergy.innerHTML = translate.pt_br.dailyEnergyExpenditure
     opMetabolic.innerHTML = translate.pt_br.basalMetabolicRate
     opNutritional.innerHTML = translate.pt_br.nutritionalNeeds
+
     StringCalories.innerHTML  = translate.pt_br.calories
     StringCarbohydrates.innerHTML = translate.pt_br.carbohydrates
     StringProtein.innerHTML  = translate.pt_br.protein
     StringFat.innerHTML  = translate.pt_br.fat
+
+    StringimcSlogan.innerHTML  = translate.pt_br.imcSlogan
+    StringimcThinness.innerHTML = "vai tomar no cu"
+    StringimcNormal.innerHTML  = translate.pt_br.imcNormal
+    StringimcOverweight.innerHTML   = translate.pt_br.imcOverweight
+    StringimcObesity.innerHTML  = translate.pt_br.imcObesity
+    StringimcObesityS.innerHTML  = translate.pt_br.imcObesityS
+    Stringinvalid.innerHTML = translate.pt_br.invalid
+    
 
     for(const i of StringBtCalculate){
         i.innerHTML= translate.pt_br.submit
@@ -184,20 +225,32 @@ function TranslateEn(){
     titleOptionsCalculator.innerHTML = translate.en.typeCalculator
     loading.innerHTML = translate.en.setCalculator
     loadingResults.innerHTML = translate.en.results
+
     titleCalculatorImc.innerHTML = translate.en.imc
     titleCalculatorDensity.innerHTML = translate.en.corporalDensity
     titleCalculatorEnergy.innerHTML = translate.en.dailyEnergyExpenditure
     titleCalculatorMetabolic.innerHTML = translate.en.basalMetabolicRate
     titleCalculatorNutritional.innerHTML = translate.en.nutritionalNeeds
+
     opIMC.innerHTML = translate.en.imc
     opDensity.innerHTML = translate.en.corporalDensity
     opEnergy.innerHTML = translate.en.dailyEnergyExpenditure
     opMetabolic.innerHTML = translate.en.basalMetabolicRate
     opNutritional.innerHTML = translate.en.nutritionalNeeds
+
     StringCalories.innerHTML  = translate.en.calories
     StringCarbohydrates.innerHTML = translate.en.carbohydrates
     StringProtein.innerHTML  = translate.en.protein
     StringFat.innerHTML  = translate.en.fat
+
+    StringimcSlogan.innerHTML  = translate.en.imcSlogan
+    StringimcThinness.innerHTML = translate.en.imcThinness
+    StringimcNormal.innerHTML  = translate.en.imcNormal
+    StringimcOverweight.innerHTML = translate.en.imcOverweight
+    StringimcObesity.innerHTML = translate.en.imcObesity
+    StringimcObesityS.innerHTML = translate.en.imcObesityS
+    Stringinvalid.innerHTML = translate.en.invalid
+
     for(const i of StringBtCalculate){
         i.innerHTML= translate.en.submit
     }
@@ -280,41 +333,43 @@ opNutritional.addEventListener("click", () => {
 })
 
 // Calculator operation
-calculator.addEventListener("submit", (event) => event.preventDefault())
 
 
-function CalculateImc() {
+
+calculatorImc.addEventListener("submit", (event) => {
+   event.preventDefault()
    const height = parseFloat(document.querySelector(" #CalculatorIMC #height").value) 
    const weight = parseFloat(document.querySelector("#CalculatorIMC #weight").value)
    let result
 
    if(isNaN(height))
     {
-        result = invalid
+        result = Stringinvalid
     }else if(isNaN(weight))
     {
-        result = invalid
+        result = Stringinvalid
     }else
 
      {  let resultImc = parseInt(weight / (height * height)) 
         let imcGrau 
 
         if(resultImc < 18.5){
-            imcGrau = "Magreza"
+            imcGrau = StringimcThinness
         }else if( resultImc <= 24.9) {
-            imcGrau = "Normal"
+            imcGrau = StringimcNormal
         }else if(resultImc <= 29.9){
-            imcGrau = "Sobrepeso"
+            imcGrau = StringimcOverweight
         }else if( resultImc <= 39.9){
-            imcGrau = "Obsidade"
+            imcGrau = StringimcObesity
         }else{
-            imcGrau = "Obesidade grave"
+            imcGrau = StringimcObesityS
         }
         
-        result = `Seu IMC é ${resultImc} - ${imcGrau}`
+        result = `${StringimcSlogan} ${resultImc} - ${imcGrau}`
     }
-    console.log(result)
-}
+    
+    ResutsAplication(result)
+})
 
 function CalculateDensity(){
     const age = parseInt(document.querySelector(" #age").value) 
@@ -326,7 +381,9 @@ function CalculateDensity(){
  
 }
 
-function CalculateEnergy(){
+calculatorEnergy.addEventListener("submit", (event) => {
+
+    event.preventDefault()
     const height = parseFloat(document.querySelector(" #CalculatorEnergy #height").value) 
     const weight = parseFloat(document.querySelector(" #CalculatorEnergy #weight").value)
     const age = parseInt(document.querySelector("#CalculatorEnergy #age").value)
@@ -334,10 +391,11 @@ function CalculateEnergy(){
     const optionActivity = document.getElementsByName('activity')
     let valorGender
     let valorActvity
+    let result = 0
 
     for(const op of optionGender){
         if(op.checked){
-            valorSexo = op.value
+            valorGender = op.value
         }
     }
 
@@ -346,6 +404,94 @@ function CalculateEnergy(){
             valorActvity = op.value
         }
     }
-    console.log(height, weight, age, valorSexo, valorActvity)
-}
+    
+    if( !isNaN(height) && !isNaN(weight) && !isNaN(age) && typeof(valorGender) !== 'undefined' && typeof(valorActvity) !== 'undefined') {
+        
 
+        if(valorGender == 'masculine'){
+            result = 10 * weight + 6.25 * (height * 100) - 5 * age + 5
+        }else{
+            result = 10 * weight + 6.25 * (100 *height )- 5 * age + 161
+        }
+        switch(valorActvity){
+            case "activityS":
+                result *= 1.2
+                break
+            case "activityL":
+                result *= 1.50
+                break
+            case "activityM":
+                result *= 1.6
+                break
+            case "activityV":
+                result  *= 1.8
+                break
+            case "activityE":
+                result *= 2.2
+                break
+        }
+
+        result = `Seus gasto energético diário é de ${result} kcal`
+
+    }else{
+        result = Stringinvalid
+    }
+    ResutsAplication(result)
+    
+   
+})
+calculatorMetabolic.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const height = parseFloat(document.querySelector(" #CalculatorMetabolic #height").value) 
+    const weight = parseFloat(document.querySelector(" #CalculatorMetabolic #weight").value)
+    const age = parseInt(document.querySelector("#CalculatorMetabolic #age").value)
+    const optionGender = document.getElementsByName('gender')
+    let valorGender
+    let result = 0
+    
+    for(const op of optionGender){
+        if(op.checked){
+            valorGender = op.value
+        }
+    }
+    
+    if( !isNaN(height) && !isNaN(weight) && !isNaN(age) && typeof(valorGender) !== 'undefined') {
+        
+        if(valorGender == 'masculine'){
+            result = 66 + (13.7 * weight) + 5 * (height * 100) - ( age * 6.8)
+        }else{
+            result = 655 + (9.6 * weight) + 1.8 * (height * 100) - ( age * 4.7)
+        }
+        result = `Seus TBM é de ${result} kcal`
+
+    }else{
+        result = Stringinvalid
+    }
+
+    ResutsAplication(result)
+})
+
+calculatorNutritional.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const calories = parseFloat(document.querySelector("#calories").value)
+    const carbohydrates = parseFloat(document.querySelector("#carbohydrates").value)
+    const protein = parseFloat(document.querySelector("#protein").value)
+    const fat = parseFloat(document.querySelector("#fat").value)
+    let result
+
+    let tCarb = parseInt((calories / 4 /  (carbohydrates * 100)) * 1000)
+    let tProtein = parseInt((calories / 4 / (protein * 100)) * 1000)
+    let tFat = parseInt((calories / 9 / (fat * 100)) * 1000)
+
+    result = `Carb: ${tCarb}g </br> Protein: ${tProtein}g</br> Fat: ${tFat}g`
+    ResutsAplication(result)
+})
+
+
+function ResutsAplication(result){
+    pResult.innerHTML = " " 
+    loadingResults.classList.add("disabled")
+    pResult. innerHTML = result
+    results.appendChild(pResult)
+}
